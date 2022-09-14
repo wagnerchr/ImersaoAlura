25ada5efab7e3bf5f7da2c9da6f4fd671e0c260b
WagnerChr <wagnerckv@gmail.com>
2022-09-13 23:12:54 -0300

    added other coin options

--- ConversorMoedas/index.js
+++ ConversorMoedas/index.js
@@ -1,19 +1,46 @@
 function Converter() {
+
+    // Selecionar campo
     let valorElemento = document.getElementById("valor");
+    // Pegar valor digitado
     let valor = valorElemento.value;
-    const valorDolar =
-         parseFloat(valor)
-    const cotacao = 
-        5
+    // Converter para float
+    const valorDolar = parseFloat(valor);
+  
+// Moeda escolhida 
+    const btnSelected = document.getElementById("checkCoin");
+    const escolhaConversao = btnSelected.innerText.substr(13)
+    console.log(escolhaConversao);
+    
 
-    const valorReal = valorDolar * 
-        cotacao
+    const valorReal = valorDolar * EscolherMoeda(escolhaConversao);
 
-    console.log("Valor digitado em U$: " +
-         valorDolar)
-    console.log("Valor convertido em R$: " + valorReal);
 
     let valorConversao = document.getElementById("valorConvertido");
     const valorConvertido = "A conversão em real é R$ " + valorReal;
     valorConversao.innerHTML = valorConvertido;    
 }
+
+
+function EscolherMoeda(evt) {
+
+    const btnSelected = document.getElementById("checkCoin")
+    btnSelected.innerHTML = "Selecionado: " // Limpando
+    btnSelected.innerHTML += evt;
+
+    let cotacao;
+
+    if(evt == "dolar")
+        cotacao = 5.19
+    else if(evt == "libra") 
+        cotacao = 5.97
+    else if(evt == "yen")
+        cotacao = 0.036
+    else {
+        console.log("deu erro aqui :/")
+        cotacao = 0;
+    }
+
+    return cotacao;
+}
+
