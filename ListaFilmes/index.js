357cf0302530cc08df0a581373ced0e9ede0f5fb
WagnerChr <wagnerckv@gmail.com>
2022-09-16 15:41:45 -0300

    Lista Filems

---
+++ ListaFilmes/index.js
@@ -0,0 +1,8 @@
+const listaFilmes = ["https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg",
+"https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg",
+"https://upload.wikimedia.org/wikipedia/pt/1/1b/Schoolrockposter.jpg",
+]
+
+for(filme of listaFilmes) {
+    document.write("<img src="+ filme + ">")
+}
