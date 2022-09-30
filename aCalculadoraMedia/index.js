d07a1b4c877768ac0dd8fafa93c4d8ce56866e73
WagnerChr <wagnerckv@gmail.com>
2022-09-30 12:33:10 -0300

    Calc

---
+++ aCalculadoraMedia/index.js
@@ -0,0 +1,18 @@
+var nome = "Guilherme"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2 
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// isso é um comentário 
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
