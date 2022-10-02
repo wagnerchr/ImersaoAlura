629dd9413754b7cf6ce79a107d4cdb5d06cea47c
WagnerChr <wagnerckv@gmail.com>
2022-10-02 15:47:58 -0300

    with results now

--- aCalculadoraMedia/index.js
+++ aCalculadoraMedia/index.js
@@ -21,10 +21,63 @@ function totalTests() {
     // Adiciona box-container para as provas
         let body = document.body;
         let box = document.createElement('div');
+        let confirmButton = document.createElement('button');
+        let resultadoMedia = document.createElement('h1');
+        let veredito = document.createElement('h3');
+        
+
+
         box.classList.add("test-box");
+        resultadoMedia.classList.add("resultado-media")
+
+        veredito.classList.add("resultado-media");
+        
+        confirmButton.classList.add("test-btn");
+        confirmButton.type = "submit";
+        confirmButton.innerHTML = "Confirmar Notas";
+
+        confirmButton.onclick= function confirmTests() {
+
+            let inputValuesArr = [];
+
+            for(let i = 1; i <= qntTestsValue; i++) {
+
+                let inputId = "eInput" + i
+                let inputValue = document.getElementById(inputId);
+                
+
+                
+                inputValuesArr.push(parseInt(inputValue.value));
+            }
+
+            let media = 0;
+
+            for(let i = 0; i < qntTestsValue; i++) {
+                media += inputValuesArr[i]
+            }
+
+            let resultado = Math.round(media/qntTestsValue);
+
+            resultadoMedia.innerHTML = "Média: " + resultado;
+            resultadoMedia.style.display = "block";
+            confirmButton.disabled = "disabled";
+
+            veredito.style.display = "block";
+
+            if(resultado > 6)
+                veredito.innerHTML = "Aprovado! :)"
+            else
+                veredito.innerHTML = "Não foi aprovado :("
+        };
+
+       
+
         body.classList.add("container");
 
         body.appendChild(box);
+        body.appendChild(confirmButton)
+        body.appendChild(resultadoMedia)
+        body.appendChild(veredito)
 
     // Loop para cada Prova Inserida
         for(let i = 1; i <= qntTestsValue; i++) {
@@ -32,24 +85,30 @@ function totalTests() {
             let e = document.createElement('div');
             let eLabel = document.createElement('label');
             let eInput = document.createElement('input')
+            
             eInput.type = 'text';
-            eInput.maxLength='3';
+            eInput.maxLength='2';
         
                 eLabel.classList.add("test-label");
                 eInput.classList.add("test-input");
+                eInput.setAttribute("id", "eInput" + i)
 
 
             eLabel.innerHTML = "prova " + i;
             eInput.innerHTML = ""
-
             
 
-            
+
             e.classList.add("test-div");
             e.appendChild(eLabel)
             e.appendChild(eInput)
             box.appendChild(e)
         }
+
+    function confirmTests() {
+        console.log("oi :3")
+      
+    }
 
     
 
