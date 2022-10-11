859ec0a7559bb319b303ef7529365457037a8896
WagnerChr <wagnerckv@gmail.com>
2022-10-11 15:32:28 -0300

    filme repetido

--- cListaFilmes/index.js
+++ cListaFilmes/index.js
@@ -1,28 +1,31 @@
 
-
 const listaFilmes = ["https://upload.wikimedia.org/wikipedia/pt/7/79/Yesterday_%282019%29_poster.jpg",
 "https://upload.wikimedia.org/wikipedia/en/d/df/Arrival%2C_Movie_Poster.jpg",
-"https://upload.wikimedia.org/wikipedia/pt/1/1b/Schoolrockposter.jpg",
+"https://upload.wikimedia.org/wikipedia/pt/1/1b/Schoolrockposter.jpg"
 ]
 
-function addFilme() {
+Atualiza();
 
-    listaFilmes.push(filme);
+function Atualiza() {
 
-}
-
-
-
-// Exibir Filmes
+    
     for(filme of listaFilmes) {
 
-        let film = document.createElement('div')
-        film.classList.add("filme");
-        film.innerHTML = `<img src="${filme}"/>`
+   
+       
 
+
+     
+        let film = document.createElement('div');   
+        film.classList.add("filme");
+
+        film.innerHTML = `<img src="${filme}"/>`
         document.getElementById("filmes").appendChild(film); 
-        
+  
     }
+}
+// Exibir Filmes
+    
 
 const view = document.createElement('div');
 const mensagem = document.createElement('h1'); 
@@ -38,8 +41,15 @@ function displayFilme(link) {
         document.getElementById("add-filme").appendChild(mensagem);
         document.getElementById("add-filme").appendChild(view);
             
-    }
+    } // https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg
         
+}
+
+function addFilme() {
+    const Input = document.getElementById('add-input').value;
+    listaFilmes.push(Input);
+
+    Atualiza();
 }
 
 
